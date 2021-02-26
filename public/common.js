ad9362279afaffcf71f8dfb65ba2b24510d0ab26
masato <maboshiva@gmail.com>
2021-02-26 23:25:11 +0900

    first commit

---
+++ public/common.js
@@ -0,0 +1,5 @@
+export default {
+  store: [
+    { image: "https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/sushi.jpg", name: "仙人", location: "東京都", category: "寿司" },
+    {image: "https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/yakiniku.jpg", name: "牛助", location: "大阪", category: "焼肉"}]
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="card">
+      <div class="cardwrap" v-for="(store, index) in storeData" :key="index">
+        <img alt="Vue logo" :src="store.image" class="thumbnailsImage" />
+        <div class="sotreName_Location_detailButton">
+          <p class="storeName">{{ store.name }}</p>
+          <p class="storeLocation_category">
+            {{ store.location }} / {{ store.category }}
+          </p>
+          <div class="button_icon">
+            <button class="detailBUtton">詳しく見る</button>
+            <i class="fas fa-heart fa-2x heartIcon"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import common from "../../public/common";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      storeData: common.store,
+    };
+  },
+
+  created() {
+    console.log(this.storeData);
+  },
 };
 </script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.cardwrap {
+  width: 20%;
+  margin-right: 15px;
+  border: 1px solid black;
+}
+.thumbnailsImage {
+  height: 150px;
+  width: 100%;
+  display: block;
+}
+.sotreName_Location_detailButton {
+  text-align: left;
+  margin-left: 15px;
+  margin-bottom: 15px;
+}
+.storeName {
+  font-size: 16px;
+  font-weight: 600;
+}
+.detailBUtton {
+  background-color: blue;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  width: 100px;
+}
+.button_icon {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  width: 95%;
+}
+</style>
